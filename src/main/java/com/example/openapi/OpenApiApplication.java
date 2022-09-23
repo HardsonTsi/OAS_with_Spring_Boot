2b19ebf75aa995377cd154714a4175f46e494ae3
Your Name <hardsontessi2@gmail.com>
2022-09-23 10:40:19 +0100

    project name and servers

--- src/main/java/com/example/openapi/OpenApiApplication.java
+++ src/main/java/com/example/openapi/OpenApiApplication.java
@@ -1,9 +1,16 @@
 package com.example.openapi;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(title = "TodoItem API", version = "1.0.0"),
+        servers = {@Server(url = "http://localhost:8080"), @Server(url = "todoitem-api.bj")}
+)
 public class OpenApiApplication {
 
     public static void main(String[] args) {
