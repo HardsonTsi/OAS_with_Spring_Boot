47eb6ee9026a0aab72f6210840fac0fccebb4ccb
Your Name <hardsontessi2@gmail.com>
2022-09-23 11:57:52 +0100

    define request body

--- src/main/java/com/example/openapi/controllers/UpdateToDoItem.java
+++ src/main/java/com/example/openapi/controllers/UpdateToDoItem.java
@@ -1,7 +1,10 @@
 package com.example.openapi.controllers;
 
+import com.example.openapi.models.Customer;
 import com.example.openapi.models.ToDoItem;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,7 +15,13 @@ public class UpdateToDoItem {
             tags = {"TodoItems", "TodoItem2"},
             operationId = "updateTodo",
             summary = "This is summary",
-            description = "This is the description"
+            description = "This is the description",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "This is request body descritpion.",
+                    content = {
+                            @Content(schema = @Schema(implementation = Customer.class))
+                    }
+            )
     )
     public ResponseEntity<Object> updateToDoItem(@PathVariable String toDoItemId,
                                                  @RequestBody ToDoItem toDoItem,
---
+++ src/main/java/com/example/openapi/models/Customer.java
@@ -0,0 +1,9 @@
+package com.example.openapi.models;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class Customer {
+    public String customerId;
+
+}
