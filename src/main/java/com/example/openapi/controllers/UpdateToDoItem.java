dcde5b8afea63e5d8918c33ac4eb080db31b445e
Your Name <hardsontessi2@gmail.com>
2022-09-23 14:18:16 +0100

    secure UpdateToDoItem (updateToDoItem controller)

--- src/main/java/com/example/openapi/controllers/UpdateToDoItem.java
+++ src/main/java/com/example/openapi/controllers/UpdateToDoItem.java
@@ -8,6 +8,7 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -48,6 +49,14 @@ public class UpdateToDoItem {
                                                     value = "String")
                                     }
                             )
+                    )
+            },
+            security = {
+                    @SecurityRequirement(
+                            name = "Bearer",
+                            scopes = {
+
+                            }
                     )
             }
     )
