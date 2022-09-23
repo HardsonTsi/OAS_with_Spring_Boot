cec8a3f22828986cc6dcce1023729402d1940c28
Your Name <hardsontessi2@gmail.com>
2022-09-23 14:10:17 +0100

    authentication

--- src/main/java/com/example/openapi/OpenApiApplication.java
+++ src/main/java/com/example/openapi/OpenApiApplication.java
@@ -1,7 +1,9 @@
 package com.example.openapi;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.boot.SpringApplication;
@@ -12,6 +14,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
         info = @Info(title = "TodoItem API", version = "1.0.0"),
         servers = {@Server(url = "http://localhost:8080"), @Server(url = "todoitem-api.bj")},
         tags = {@Tag(name = "TodoItems", description = "This is todoItems description.")}
+)
+
+@SecurityScheme(
+        name = "BearerJWT",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT",
+        description = "Bearer token for the project."
 )
 public class OpenApiApplication {
 
